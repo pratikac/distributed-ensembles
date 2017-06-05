2df2f409e60a01587c08482a6a55d8b678e8cfad
pratik.ac <pratik.ac@gmail.com>
2017-06-05 05:10:28 -0700

    minor

--- code/plotlog.py
+++ code/plotlog.py
@@ -140,17 +140,18 @@ def allcnn():
         plt.savefig('../fig/cifar_full_valid.pdf', bbox_inches='tight')
 
     sgd['frac'].replace(1.0, 0.5, inplace=True)
-    f = rough(pd.concat([df[df['frac'] == 0.5], sgd]), 1)
+    f = rough(pd.concat([df[df['frac'] == 0.5], sgd]), 2)
     plt.figure(f.number)
     plt.title('CIFAR-10: frac=0.5')
     plt.xlabel('epochs x L')
     plt.xlim([0, 200])
     plt.ylim([6, 15])
+    set_ticks(xt=[0, 50, 100, 150, 200], yt=[6,9,12,15])
     if opt['s']:
         plt.savefig('../fig/cifar_half_valid.pdf', bbox_inches='tight')
 
     sgd['frac'].replace(0.5, 0.25, inplace=True)
-    f = rough(pd.concat([df[df['frac'] == 0.25], sgd]), 1)
+    f = rough(pd.concat([df[df['frac'] == 0.25], sgd]), 3)
     plt.figure(f.number)
     plt.title('CIFAR-10: frac=0.25')
     plt.xlabel('epochs x L')
