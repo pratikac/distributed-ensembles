0261822b371fbba4b70b9ebec3cf3adc5ff9f282
pratik.ac <pratik.ac@gmail.com>
2017-06-08 01:53:26 -0700

    updated plots

--- code/plotlog.py
+++ code/plotlog.py
@@ -60,7 +60,7 @@ whitelist = ['n', 'L', 'e',
             'train','val',
             'frac']
 colors = {  'SGD':'k', 'SGD (full)':'m',
-            'ESGD':'r', 'Dist-ESGD (n=3)':'b', 'Dist-ESGD (n=6)':'g'}
+            'Entropy-SGD':'r', 'Dist-ESGD (n=3)':'b', 'Dist-ESGD (n=6)':'g'}
 
 df = loaddir(os.path.join(opt['l'], opt['m']), force=opt['f'])
 df = df[(df['summary'] == True)]
@@ -77,7 +77,7 @@ df['ee'] = df['e']*df['L']*df['frac']
 
 df['optim'] = 'GD'
 df.ix[(df['L'] ==1) & (df['n'] == 1), 'optim'] = 'SGD'
-df.ix[(df['L'] !=1) & (df['n'] == 1), 'optim'] = 'ESGD'
+df.ix[(df['L'] !=1) & (df['n'] == 1), 'optim'] = 'Entropy-SGD'
 for ni in np.unique(df.n):
     if ni != 1:
         df.ix[(df['L'] !=1) & (df['n'] == ni), 'optim'] = 'Dist-ESGD (n=%d)'%(ni)
@@ -126,6 +126,15 @@ def lenet():
     plt.xlim([0, 100])
     plt.ylim([0.4, 1.0])
     plt.xlabel('epochs x L')
+
+    plt.text(75, 0.42, r'$0.45$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Dist-ESGD (n=6)'])
+    plt.text(85, 0.56, r'$0.49$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Entropy-SGD'])
+    plt.text(70, 0.56, r'$0.50$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Dist-ESGD (n=3)'])
+    plt.text(70, 0.60, r'$0.50$%', fontsize=fsz,
+        verticalalignment='center', color=colors['SGD'])
     if opt['s']:
         plt.savefig('../fig/lenet_full_valid.pdf', bbox_inches='tight')
 
@@ -136,6 +145,12 @@ def allcnn_cifar10():
     plt.xlabel('epochs x L')
     plt.xlim([50, 300])
     plt.ylim([4, 16])
+    plt.text(225, 6.75, r'$5.75$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Entropy-SGD'])
+    plt.text(275, 6, r'$5.18$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Dist-ESGD (n=3)'])
+    plt.text(175, 7.65, r'$6.14$%', fontsize=fsz,
+        verticalalignment='center', color=colors['SGD'])
     if opt['s']:
         plt.savefig('../fig/allcnn_cifar10_full_valid.pdf', bbox_inches='tight')
 
@@ -147,6 +162,16 @@ def allcnn_cifar10():
     plt.xlim([0, 200])
     plt.ylim([6, 15])
     set_ticks(xt=[0, 50, 100, 150, 200], yt=[6,9,12,15])
+
+    plt.text(85, 8.65, r'$7.98$%', fontsize=fsz,
+        verticalalignment='center', color=colors['SGD'])
+    plt.text(125, 8, r'$7.35$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Entropy-SGD'])
+    plt.text(95, 6.7, r'$6.08$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Dist-ESGD (n=3)'])
+    plt.text(175, 6.8, r'$6.14$%', fontsize=fsz,
+        verticalalignment='center', color=colors['SGD (full)'])
+
     if opt['s']:
         plt.savefig('../fig/allcnn_cifar10_half_valid.pdf', bbox_inches='tight')
 
@@ -159,6 +184,15 @@ def allcnn_cifar10():
     plt.ylim([6, 18])
     set_ticks(xt=[0, 50, 100, 150, 200], yt=[6,10,14,18])
 
+    plt.text(35, 12, r'$10.87$%', fontsize=fsz,
+        verticalalignment='center', color=colors['SGD'])
+    plt.text(40, 9.22, r'$9.69$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Entropy-SGD'])
+    plt.text(50, 7.5, r'$6.69$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Dist-ESGD (n=6)'])
+    plt.text(175, 6.8, r'$6.14$%', fontsize=fsz,
+        verticalalignment='center', color=colors['SGD (full)'])
+
     if opt['s']:
         plt.savefig('../fig/allcnn_cifar10_fourth_valid.pdf', bbox_inches='tight')
 
@@ -170,6 +204,14 @@ def allcnn_cifar100():
     plt.xlim([0, 200])
     plt.ylim([20, 70])
     set_ticks(xt=[0, 50, 100, 150, 200])
+
+    plt.text(170, 34, r'$26.95$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Entropy-SGD'])
+    plt.text(170, 23.4, r'$25.19$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Dist-ESGD (n=3)'])
+    plt.text(140, 33, r'$28.99$%', fontsize=fsz,
+        verticalalignment='center', color=colors['SGD'])
+
     if opt['s']:
         plt.savefig('../fig/allcnn_cifar100_full_valid.pdf', bbox_inches='tight')
 
@@ -201,10 +243,35 @@ def wrn_cifar10():
     plt.title('Wide-ResNet: CIFAR-10')
     plt.xlabel('epochs x L')
     plt.xlim([0, 200])
-    plt.ylim([20, 70])
+    plt.ylim([0, 50])
     set_ticks(xt=[0, 50, 100, 150, 200])
+
+    plt.text(120, 1.5, r'$3.24$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Dist-ESGD (n=3)'])
+    plt.text(150, 6.5, r'$4.62$%', fontsize=fsz,
+        verticalalignment='center', color=colors['SGD'])
+
     if opt['s']:
         plt.savefig('../fig/wrn_cifar10_full_valid.pdf', bbox_inches='tight')
+
+def wrn_cifar100():
+    f = rough(df[df['frac'] == 1], 1)
+    plt.figure(f.number)
+    plt.title('Wide-ResNet: CIFAR-100')
+    plt.xlabel('epochs x L')
+    plt.xlim([0, 200])
+    plt.ylim([15, 45])
+    set_ticks(xt=[0, 50, 100, 150, 200], yt=[15,25,35,45])
+
+    plt.text(90, 16.5, r'$17.44$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Dist-ESGD (n=3)'])
+    plt.text(175, 18, r'$19.01$%', fontsize=fsz,
+        verticalalignment='center', color=colors['Entropy-SGD'])
+    plt.text(130, 18, r'$19.5$%', fontsize=fsz,
+        verticalalignment='center', color=colors['SGD'])
+
+    if opt['s']:
+        plt.savefig('../fig/wrn_cifar100_full_valid.pdf', bbox_inches='tight')
 
 
 globals()[opt['m']]()
