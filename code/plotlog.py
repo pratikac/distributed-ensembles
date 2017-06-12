90d2d6c60b6ddaaaa76caa8558806b5c566ebf39
pratik.ac <pratik.ac@gmail.com>
2017-06-11 23:35:55 -0400

    minor on plotting

--- code/plotlog.py
+++ code/plotlog.py
@@ -263,7 +263,18 @@ def wrn_cifar10():
     # plt.ylim([6, 15])
     # set_ticks(xt=[0, 50, 100, 150, 200], yt=[6,9,12,15])
     # if opt['s']:
-    #     plt.savefig('../fig/allcnn_cifar100_half_valid.pdf', bbox_inches='tight')
+    #     plt.savefig('../fig/wrn_cifar10_half_valid.pdf', bbox_inches='tight')
+
+    sgd['frac'].replace(0.5, 0.25, inplace=True)
+    f = rough(pd.concat([df[df['frac'] == 0.25], sgd]), 3)
+    plt.figure(f.number)
+    plt.title('Wide-ResNet: CIFAR-10 (frac = 0.25)')
+    plt.xlabel('epochs x L x frac')
+    # plt.xlim([0, 200])
+    # plt.ylim([6, 18])
+    # set_ticks(xt=[0, 50, 100, 150, 200], yt=[6,10,14,18])
+    # if opt['s']:
+    #     plt.savefig('../fig/wrn_cifar10_fourth_valid.pdf', bbox_inches='tight')
 
 def wrn_cifar100():
     f = rough(df[df['frac'] == 1], 1)
@@ -281,9 +292,29 @@ def wrn_cifar100():
     plt.text(130, 18, r'$19.5$%', fontsize=fsz,
         verticalalignment='center', color=colors['SGD'])
 
-
     if opt['s']:
         plt.savefig('../fig/wrn_cifar100_full_valid.pdf', bbox_inches='tight')
 
+    sgd['frac'].replace(1.0, 0.5, inplace=True)
+    f = rough(pd.concat([df[df['frac'] == 0.5], sgd]), 2)
+    plt.figure(f.number)
+    plt.title('Wide-ResNet: CIFAR-100 (frac = 0.5)')
+    plt.xlabel('epochs x L x frac')
+    # plt.xlim([0, 200])
+    # plt.ylim([6, 15])
+    # set_ticks(xt=[0, 50, 100, 150, 200], yt=[6,9,12,15])
+    # if opt['s']:
+    #     plt.savefig('../fig/wrn_cifar100_half_valid.pdf', bbox_inches='tight')
+
+    sgd['frac'].replace(0.5, 0.25, inplace=True)
+    f = rough(pd.concat([df[df['frac'] == 0.25], sgd]), 3)
+    plt.figure(f.number)
+    plt.title('Wide-ResNet: CIFAR-100 (frac = 0.25)')
+    plt.xlabel('epochs x L x frac')
+    # plt.xlim([0, 200])
+    # plt.ylim([6, 18])
+    # set_ticks(xt=[0, 50, 100, 150, 200], yt=[6,10,14,18])
+    # if opt['s']:
+    #     plt.savefig('../fig/wrn_cifar100_fourth_valid.pdf', bbox_inches='tight')
 
 globals()[opt['m']]()
