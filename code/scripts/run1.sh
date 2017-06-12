db9d050634ca9449049c3b6a3fbbbc61d6412d62
pratik.ac <pratik.ac@gmail.com>
2017-06-11 22:12:13 -0700

    minor

---
+++ code/scripts/run1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+python hyperoptim.py -c "python ensemble.py -m wrn2810 --dataset cifar100 --lrs '[[1,0.1],[2,0.02],[3,0.004],[5,0.001]]' -B 5 --augment -d 0.25 -n 6 --frac 0.25 -b 64" -p '{"s":[42,43]}' -j 1 -r --dist
+
+python hyperoptim.py -c "python ensemble.py -m wrn2810 --dataset cifar10 --lrs '[[1,0.1],[2,0.02],[3,0.004],[5,0.001]]' -B 5 --augment -d 0.25 -n 6 --frac 0.25 -b 64" -p '{"s":[42,43]}' -j 1 -r --dist
