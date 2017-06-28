f66866271b84a6845d9a483170b9a0d41d9b93f6
pratik.ac <pratik.ac@gmail.com>
2017-06-27 22:14:05 -0700

    starting run1.sh

--- code/scripts/run1.sh
+++ code/scripts/run1.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-python hyperoptim.py -c "python ensemble.py -m wrn2810 --dataset cifar100 --lrs '[[1,0.1],[2,0.02],[3,0.004],[5,0.001]]' -B 5 --augment -d 0.25 -n 6 --frac 0.25 -b 64" -p '{"s":[42]}' -j 1 -r --dist
-python hyperoptim.py -c "python ensemble.py -m wrn2810 --dataset cifar10 --lrs '[[1,0.1],[2,0.02],[3,0.004],[5,0.001]]' -B 5 --augment -d 0.25 -n 6 --frac 0.25 -b 64" -p '{"s":[42]}' -j 1 -r --dist
+python hyperoptim.py -c "python ensemble.py -n 3 -m allcnn --dataset cifar10 --augment --lrs '[[2,0.1],[4,0.02],[6,0.004],[10,0.001]]' -B 10 --frac 0.5 -d 0.0" -p '{"s":[44]}' -j 1 --dist -r
 
-python hyperoptim.py -c "python ensemble.py -m wrn168 --dataset svhn --lrs '[[1,0.1],[2,0.02],[3,0.004],[5,0.001]]' -B 5 -d 0.4 -n 3" -p '{"s":[42]}' -j 1 -r --dist
-python hyperoptim.py -c "python ensemble.py -m wrn168 --dataset svhn --lrs '[[1,0.1],[2,0.02],[3,0.004],[5,0.001]]' -B 6 -d 0.4 -n 1" -p '{"s":[42,43,44]}' -j 3 -r
+python hyperoptim.py -c "python ensemble.py -n 6 -m allcnn --dataset cifar10 --augment --lrs '[[2,0.1],[4,0.02],[6,0.004],[10,0.001]]' -B 10 --frac 0.25 -d 0.0" -p '{"s":[44]}' -j 1 --dist -r
+
