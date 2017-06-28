d393331f6cf62b00c122fcc6f75404a33aa2857b
pratik.ac <pratik.ac@gmail.com>
2017-06-28 00:40:18 -0700

    new run1.sh

--- code/scripts/run1.sh
+++ code/scripts/run1.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-python hyperoptim.py -c "python ensemble.py -n 3 -m allcnn --dataset cifar10 --augment --lrs '[[2,0.1],[4,0.02],[6,0.004],[10,0.001]]' -B 10 --frac 0.5 -d 0.0" -p '{"s":[44]}' -j 1 --dist -r
+python hyperoptim.py -c "python ensemble.py -n 3 -m allcnn --dataset cifar10 --augment --lrs '[[60,0.1],[120,0.02],[160,0.004],[200,0.001]]' -B 200 --frac 0.5 -L 0 -d 0.0" -p '{"s":[42,43,44]}' -j 1 --dist -r
 
-python hyperoptim.py -c "python ensemble.py -n 6 -m allcnn --dataset cifar10 --augment --lrs '[[2,0.1],[4,0.02],[6,0.004],[10,0.001]]' -B 10 --frac 0.25 -d 0.0" -p '{"s":[44]}' -j 1 --dist -r
-
+python hyperoptim.py -c "python ensemble.py -n 6 -m allcnn --dataset cifar10 --augment --lrs '[[60,0.1],[120,0.02],[160,0.004],[200,0.001]]' -B 200 --frac 0.25 -L 0 -d 0.0" -p '{"s":[42,43,44]}' -j 1 --dist -r
